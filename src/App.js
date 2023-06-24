bca852c76c3eb9fdd531c4b127af976d140f2fa5
Zach Dettwiler <z.dettwiler@gmail.com>
2023-06-24 14:30:34 +0200

    basic app setup

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+// import { , ThemeProvider } from '@mui/material/styles';
+
+function App() {
+  return (
+    // <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={'Genèse'}
+                label="Livre"
+                onChange={() => {}}
+            >
+                <MenuItem value={'Genèse'}>Genèse</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Container>
+    // </ThemeProvider>
+  );
+}
+
+export default App;
