a75e76bddea3513b1a2e88a6cedf9f595fbffdb5
Zach Dettwiler <z.dettwiler@gmail.com>
2024-01-27 14:13:30 +0100

    display lexicon version

--- src/app/[[...params]]/layout.js
+++ src/app/[[...params]]/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LLB from "./LexiqueBibliqueFrancais.json";
 
 export const metadata = {
   title: 'Lexique du lecteur biblique',
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
         <footer>
           <p>🔧 par Zacharie Dettwiler en 2023<br/>
           avec les données de <a href="https://etcbc.github.io/bhsa/">BHSA</a> (<a href="https://dx.doi.org/10.17026/dans-z6y-skyh">10.17026/dans-z6y-skyh</a>), <a href="https://github.com/STEPBible/STEPBible-Data/tree/master/Translators%20Amalgamated%20OT%2BNT">THGNT</a> et <a href="https://www.levangile.com/Liste-Strong-Grec.php">Levangile</a>.</p>
+          <p>LLB {LLB.version}</p>
           <a className="discreet" href="https://github.com/zdettwiler/lexique-du-lecteur-biblique"><i className="bi bi-github"></i></a>
         </footer>
       </body>
