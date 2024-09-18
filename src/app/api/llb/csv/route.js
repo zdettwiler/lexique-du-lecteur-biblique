a52e3605bf75d7d4912ff2df5ab7b5f85126eb25
Zach Dettwiler <z.dettwiler@gmail.com>
2024-09-18 14:34:48 +0200

    download all llb as csv

--- src/app/api/llb/csv/route.js
+++ src/app/api/llb/csv/route.js
@@ -2,20 +2,20 @@ import { NextResponse } from 'next/server';
 import createSupabaseClient from '@/utils/supabase/server';
 
 
-export async function GET(request, { params: { strong }}) {
+export async function GET(request) {
   const supabase = createSupabaseClient();
-
-  // clean strong, ex: add trailing zeros, G509 -> G0509
 
   const { data, error, status } = await supabase
     .from('llb')
     .select('*')
-    .eq('strong', strong)
-    .limit(1)
-    .single()
+    .csv()
 
-  return NextResponse.json(status === 200
+  const headers = new Headers();
+  headers.set("Content-Type", "text/csv")
+  headers.set("Content-Disposition", `attachment; filename="llb.csv"`)
+
+  return new NextResponse(status === 200
     ? data
     : error,
-  { status })
+  { status, headers })
 }
