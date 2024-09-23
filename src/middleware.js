cf8a35d7c9b54e67ad61e078eadda8189d52887a
Zach Dettwiler <z.dettwiler@gmail.com>
2024-09-23 18:05:07 +0200

    clean-up

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 import { bookNames, bookChapters } from './app/[[...params]]/booksMetadata'
 
 export function middleware (request) {
-  let params = request.nextUrl.pathname.match(/\/(?<book>[^\/]*)\/(?<chapters>[\d\*\,\-]*)\/?(?<frequency>\d*)?/)
+  let params = request.nextUrl.pathname.match(/\/(?<book>[^/]*)\/(?<chapters>[\d*,-]*)\/?(?<frequency>\d*)?/)
 
   if (!params) {
     return NextResponse.redirect(new URL('/', request.url))
@@ -52,8 +52,8 @@ export function middleware (request) {
           acc.push(start)
           return acc
         } else {
-		  acc.push([start,end].join('-'))
-		}
+          acc.push(start + '-' + end)
+        }
       } else {
         chapter = parseInt(chapter.trim())
         if (chapter && chapter > maxChaptersBook) chapter = maxChaptersBook
